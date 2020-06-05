e58ed7a992cb382b6c8725ec43e1897ea40a502c
Dron2019 <mr.andrey@bigmir.net>
2020-06-05 11:45:23 +0300

    single-news, gallery

---
+++ src/assets/scripts/gulp-modules/gallery.js
@@ -0,0 +1,14 @@
+$('.gallery-slider-js').on('init', (e, t) => {
+    console.log(e, t);
+    document.querySelector('#gallery .total').innerHTML = t.slideCount;
+})
+$('.gallery-slider-js').on('beforeChange', (e, t, f) => {
+    console.log(e, t, f);
+    document.querySelector('#gallery .current').innerHTML = f + 1;
+})
+
+$('.gallery-slider-js').slick({
+    nextArrow: '.gallery-arrow-js',
+    prevArrow: false,
+    fade: true,
+});
